e3d672ed187c7bd53453a91f42a68daa1e2a1fb4
songyan-hou <815759459@qq.com>
2022-06-25 12:13:46 +0200

    es

--- neuralhedge/nn/loss.py
+++ neuralhedge/nn/loss.py
@@ -4,6 +4,8 @@ import torch
 from torch import Tensor
 from torch.nn import Module
 from torch.nn.parameter import Parameter
+import torch.nn.functional as F
+
 from abc import ABC, abstractmethod
 
 def exp_utility(input: Tensor, a: float = 1.0) -> Tensor:
@@ -11,6 +13,14 @@ def exp_utility(input: Tensor, a: float = 1.0) -> Tensor:
     f(X) = -\exp(-aX) 
     """
     return -(-a * input).exp()
+
+def value_at_risk(input: Tensor, q: float = 0.01) -> Tensor:
+    return torch.quantile(input, q, interpolation='linear')
+
+def expected_shortfall(input: Tensor, q: float = 0.01) -> Tensor:
+    VaR = value_at_risk(input, q)
+    ES = F.relu(VaR - input).mean()/q - VaR
+    return ES
 
 
 class LossMeasure(ABC, Module):
@@ -68,6 +78,32 @@ class SquareMeasure(LossMeasure):
         f(X) = -E[X]
         """
         return -input.mean()
+
+class ExpectedShortfall(LossMeasure):
+    @property
+    def q(self):
+        return self._q
+    @q.setter
+    def q(self,q: float = 0.5):
+        if not q > 0 or not q < 1:
+            raise ValueError("Risk aversion coefficient should be between 0 and 1")
+        self._q = q
+    
+    def __init__(self, q: float = 0.5) -> None:
+        super().__init__()
+        self._q = q
+
+    def forward(self, input: Tensor) -> Tensor:
+        """
+        f(X) = ES(X)
+        """
+        return expected_shortfall(input, self.q)
+
+    def cash(self, input: Tensor) -> Tensor:
+        """
+        f(X) = -E[X]
+        """
+        return -value_at_risk(input, self.q)
 
 
 def proportional_cost(holding_diff, price_now) -> Tensor:
